c5b88435cc5877a89196afb86a84751fa387bd64
Raghu <gujjalaraghu5@gmail.com>
2022-05-03 08:01:03 +0530

    first commit

---
+++ src/beta/Demo1.java
@@ -0,0 +1,18 @@
+package beta;
+
+
+import org.apache.logging.log4j.*;
+
+public class Demo1 {
+	private static Logger log =LogManager.getLogger(Demo1.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.error("Object is not present");
+		log.fatal("Object is fatal");
+		log.debug("I am debugging");
+		log.info("I am Info");
+
+	}
+
+}
